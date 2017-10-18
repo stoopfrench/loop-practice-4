b8242a0c5a266e48291df2adef400e375365a362
stoopfrench <32528005+stoopfrench@users.noreply.github.com>
2017-10-17 22:09:05 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,93 @@
+var months = [{
+    name: 'January',
+    days: 31
+},{
+    name: 'February',
+    days: 28
+},{
+    name: 'March',
+    days: 31
+},{
+    name: 'April',
+    days: 30
+},{
+    name: 'May',
+    days: 31
+},{
+    name: 'June',
+    days: 30
+},{
+    name: 'July',
+    days: 31
+},{
+    name: 'August',
+    days: 31
+},{
+    name: 'September',
+    days: 30
+},{
+    name: 'October',
+    days: 31
+},{
+    name: 'November',
+    days: 30
+},{
+    name: 'December',
+    days: 31
+}];
+
+
+// loop 4.1
+
+// for (var i = 0; i < months.length; i++) {
+
+//     for(var d = 1; d <= months[i].days; d++) {
+
+//         console.log(months[i].name + ' ' + d);
+//     }
+// }
+
+
+// loop 4.2
+
+// for (var h = 0; h < 24; h++){
+
+//     for (var m = 0; m < 60; m++) {
+
+//         for (var s = 0; s < 60; s++) {
+
+//             console.log(h + ':' + m + ':' + s);
+//         }
+//     }
+// }
+
+
+// loop 4.bonus 1 ...it almost works (:
+
+
+// for (var i = 0; i < 2; i++) {
+    
+//     for (var h = 1; h <= 12; h++) {
+
+//         for (var m = 0; m < 60; m++) {
+
+//             for (var s = 0; s < 60; s++) {
+                
+//                 if (i === 0 && h <= 12) {
+                
+//                     console.log(h + ':' + m + ':' + s + ' AM');
+
+//                 }
+
+//                 else if (i !== 0 && h <= 12) {
+                    
+//                     console.log(h + ':' + m + ':' + s + ' PM')
+                
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
+
